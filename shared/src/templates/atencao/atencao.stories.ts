06a2fd121d0e6c1d9ea08160afb47e82201047c9
vitor <vitor>
2025-07-06 22:53:38 -0300

    feat: custom msg

--- shared/src/templates/atencao/atencao.stories.ts
+++ shared/src/templates/atencao/atencao.stories.ts
@@ -7,21 +7,30 @@ const atencaoServiceFake = {
     mensagem: 'Mensagem de atenção fake'
 }
 
-class MockMyService extends AtencaoService {
-    override mensagem = 'Mensagem de atenção mockada';
-}
+// class MockMyService extends AtencaoService {
+//     override mensagem = 'Mensagem de atenção mockada';
+// }
 
 const meta: Meta<AtencaoComponent> = {
     component: AtencaoComponent,
     title: 'Templates/Atencao',
-    decorators: [
-        moduleMetadata({
-            providers: [
-                // { provide: AtencaoService, useValue: atencaoServiceFake } // mocka o service Atencao (via use value) (jeito 1)
-                { provide: AtencaoService, useClass: MockMyService } // mocka o service Atencao (via use class) (jeito 2)
-            ]
-        })
-    ]
+    args: {
+        mensagem: 'Mensagem de atenção'
+    },
+    argTypes: {
+        mensagem: {
+            control: 'text',
+            description: 'Mensagem'
+        },
+    },
+    // decorators: [
+    //     moduleMetadata({
+    //         providers: [
+    //             { provide: AtencaoService, useValue: atencaoServiceFake } // mocka o service Atencao (via use value) (jeito 1)
+    //             // { provide: AtencaoService, useClass: MockMyService } // mocka o service Atencao (via use class) (jeito 2)
+    //         ]
+    //     })
+    // ]
 };
 
 export default meta;
